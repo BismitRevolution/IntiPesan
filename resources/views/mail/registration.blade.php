f070271ae7d539025901a30077000b598802f754
cymon1997 <cybermonster82@yahoo.com>
2018-12-05 16:35:44 +0700

    added hardcoded inline css

--- resources/views/mail/registration.blade.php
+++ resources/views/mail/registration.blade.php
@@ -3,8 +3,26 @@
 @section('title', 'Registration Email')
 
 @section('content')
+<div class="row" style="margin-top: 35px;">
+    <p class="col-12">{{ $data->content }}</p>
+</div>
 <div class="row">
     <div class="col-12 col-md-6">
+        <div class="row">
+            <h5 class="col-12"><small>Registrant Data</small></h5>
+        </div>
+        <div class="row">
+            <p class="col-xs-6 col-sm-6">Name : </p>
+            <p class="col-xs-6 col-sm-6"><b>{{ $data->name }}</b></p>
+        </div>
+        <div class="row">
+            <p class="col-xs-6 col-sm-6">Company : </p>
+            <p class="col-xs-6 col-sm-6"><b>{{ $data->company }}</b></p>
+        </div>
+        <div class="row">
+            <p class="col-xs-6 col-sm-6">Event : </p>
+            <p class="col-xs-6 col-sm-6"><b>{{ $data->title }}</b></p>
+        </div>
         <div class="row">
             <h5 class="col-12"><small>Login Account</small></h5>
         </div>
@@ -25,9 +43,6 @@
     <div class="col-12 col-md-6">
         <img class="float-right qrcode" src="{{ $path }}" alt="qr_code">
     </div>
-</div>
-<div class="row" style="margin-top: 35px;">
-    <p class="col-12">{{ $data->content }}</p>
 </div>
 <div class="row">
     <h5 class="col-12"><small>Lokasi Kegiatan</small></h5>
